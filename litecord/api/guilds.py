f6c2f222db0f529d3f5906d5de7a7541e835ea77
Luna Mendes <lkmnds@gmail.com>
2017-05-04 10:35:34 -0300

    Add some dummy routes in GuildsEndpoint

--- litecord/api/guilds.py
+++ litecord/api/guilds.py
@@ -14,3 +14,37 @@ class GuildsEndpoint:
 
     async def h_post_guilds(self, request):
         pass
+
+    async def h_guilds(self, request):
+        '''
+        GuildsEndpoint.h_guilds
+
+        Handle `GET /guilds/{guild_id}`
+        '''
+        _error = await self.server.check_request(request)
+        _error_json = json.loads(_error.text)
+        if _error_json['code'] == 0:
+            return _error
+
+        guild_id = request.match_info['guild_id']
+
+        guild = self.server.guild_man.get_guild(guild_id)
+        if guild is None:
+            return _err('404: Not Found')
+
+        return _json(guild.as_json)
+
+    async def h_get_guild_channels(self, request):
+        '''
+        GuildsEndpoint.h_get_guild_channels
+
+        `GET /guilds/{guild_id}/channels`
+        '''
+        _error = await self.server.check_request(request)
+        _error_json = json.loads(_error.text)
+        if _error_json['code'] == 0:
+            return _error
+
+        guild_id = request.match_info['guild_id']
+
+        return _json('Not Implemented')
