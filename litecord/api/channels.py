fc8a4aa94fc2fcc29f10998cab70ff71e83dd9e7
Luna Mendes <lkmnds@gmail.com>
2017-05-05 14:23:17 -0300

    Add POST /channels/{channel_id}/typing

--- litecord/api/channels.py
+++ litecord/api/channels.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from aiohttp import web
 from ..utils import _err, _json, strip_user_data
 
 log = logging.getLogger(__name__)
@@ -26,8 +27,7 @@ class ChannelsEndpoint:
         #_r.add_delete('/api/channels/{channel_id}/messages/{message_id}',
         #                self.h_delete_message)
 
-        # NOTE: needs typing stuff, maybe in presence manager?
-        #_r.add_post('/api/channels/{channel_id}/typing', self.h_post_typing)
+        _r.add_post('/api/channels/{channel_id}/typing', self.h_post_typing)
 
     async def h_get_channel(self, request):
         '''
@@ -52,3 +52,30 @@ class ChannelsEndpoint:
             return _err('401: Unauthorized')
 
         return _json(channel.as_json)
+
+    async def h_post_typing(self, request):
+        '''
+        ChannelsEndpoint.h_post_typing(request)
+
+        Handle `POST /channels/{channel_id}/typing`
+        Dispatches TYPING_START events to relevant clients
+        '''
+
+        _error = await self.server.check_request(request)
+        _error_json = json.loads(_error.text)
+        if _error_json['code'] == 0:
+            return _error
+
+        channel_id = request.match_info['channel_id']
+        user = self.server._user(_error_json['token'])
+
+        channel = self.server.guild_man.get_channel(channel_id)
+        if channel is None:
+            return _err('404: Not found')
+
+        if user.id not in channel.guild.members:
+            return _err('401: Unauthorized')
+
+
+        await self.server.presence.typing_start(user.id, channel_id)
+        return web.Response(status=204)
