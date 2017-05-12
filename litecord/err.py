410dee1e1cbafe71c1acdc4db42160dae2f7591d
Luna Mendes <lkmnds@gmail.com>
2017-05-12 12:47:18 -0300

    images: add implementation for avatar_register

---
+++ litecord/err.py
@@ -0,0 +1,3 @@
+
+class ImageError(Exception):
+    pass
--- litecord/images.py
+++ litecord/images.py
@@ -1,5 +1,15 @@
+import logging
 import base64
 import hashlib
+
+from .err import ImageError
+
+log = logging.getLogger(__name__)
+
+AVATAR_MIMETYPES = [
+    'image/jpeg', 'image/png',
+    #'image/gif'
+]
 
 class Images:
     """Images - image manager.
@@ -10,9 +20,53 @@ class Images:
         self.server = server
         self.config = config
 
+        self.image_db = server.litecord_db['images']
+
+    def extract_uri(self, data):
+        try:
+            sp = avatar_data.split(',')
+            data_string = sp[0]
+            encoded_data = sp[1]
+            mimetype = data_string.split(';')[0].split(':')[1]
+        except:
+            raise ImageError('error decoding image data')
+
+        return encoded_data, mimetype
+
     async def avatar_register(self, avatar_data):
-        # TODO: parse data
-        return ''
+        """Registers an avatar in the avatar database.
+
+        Returns a string, representing the image hash.
+        The image hash can be used in `Images.avatar_retrieve` to get
+        raw binary data.
+        """
+        try:
+            decoded_data, mimetype = self.extract_uri(avatar_data)
+        except ImageError as err:
+            raise err
+
+        try:
+            AVATAR_MIMETYPES.index(mimetype)
+        except:
+            raise ImageError('Invalid MIME type')
+
+        try:
+            dec_data = base64.b64decode(encoded_data)
+        except:
+            raise ImageError('Error decoding Base64 data')
+
+        data_hash = hashlib.sha256(decoded_data).hexdigest()
+
+        log.info(f'Inserting {len(dec_data)}-bytes avatar.')
+
+        await self.image_db.insert_one({
+            'type': 'avatar',
+            'hash': data_hash,
+            'data': encoded_data,
+        })
+
+        return data_hash
 
     async def avatar_retrieve(self, avatar_hash):
-        return ''
+        img = await self.image_db.find_one({'type': 'avatar', 'hash': avatar_hash})
+        return img.get('data')
