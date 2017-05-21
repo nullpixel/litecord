a7eb917ed0cfd4a320af2967ebcc825773d24823
Luna Mendes <lkmnds@gmail.com>
2017-05-21 04:13:17 -0300

    embedder: Add GET:/embed

--- litecord/embedder.py
+++ litecord/embedder.py
@@ -3,11 +3,31 @@ import aiohttp
 from bs4 import BeautifulSoup
 
 from .embeds import Embed
+from .utils import _json, _err
 
 class EmbedManager:
     def __init__(self, server):
         self.server = server
         self.cache = {}
+
+    def init(self, app):
+        _r = app.router
+
+        _r.add_get('/embed', self.h_get_embed)
+
+    async def h_get_embed(self, request):
+        """Convert a page to an embed object."""
+        try:
+            payload = await request.json()
+        except:
+            return _err('erroneous payload')
+
+        url = payload['url']
+
+        if url not in self.cache:
+            return _json((await self.url_to_embed(url)).as_json)
+
+        return _json(self.cache[url].as_json)
 
     async def url_to_embed(url):
         soup = None
@@ -24,5 +44,4 @@ class EmbedManager:
         })
 
         self.cache[url] = em
-
         return em
