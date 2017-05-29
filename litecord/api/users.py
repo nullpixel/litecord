ad51653bd1189ae41a06a58112eb967c6cd3f265
Luna Mendes <lkmnds@gmail.com>
2017-05-29 15:50:02 -0300

    api.users: use auth_route decorator

--- litecord/api/users.py
+++ litecord/api/users.py
@@ -6,6 +6,7 @@ import json
 import logging
 from ..utils import _err, _json, strip_user_data, get_random_salt, pwd_hash
 from ..snowflake import get_snowflake
+from ..decorators import auth_route
 
 log = logging.getLogger(__name__)
 
@@ -24,35 +25,30 @@ class UsersEndpoint:
         #_r.add_get('/api/users/@me/guilds', server.h_users_me_guild)
         #_r.add_delete('/api/users/@me/guilds/{guild_id}', server.h_users_guild_delete)
 
-    async def h_users(self, request):
+    @auth_route
+    async def h_users(self, request, user):
         """Handle `GET /users/{user_id}`.
 
         Get a specific user.
         """
-        _error = await self.server.check_request(request)
-        _error_json = json.loads(_error.text)
-        if _error_json['code'] == 0:
-            return _error
 
         user_id = request.match_info['user_id']
-        user = self.server._user(_error_json['token'])
-        log.info(f"token={_error_json['token']} user={user} cache={self.server.cache['token->userid']}")
+        log.debug(f"user={user} cache={self.server.cache['token->userid']}")
 
         if user_id == '@me':
             return _json(user.as_json)
         else:
             # If we want to be full discord-like, uncomment this
-            #if not user['bot']:
+            #if not user.bot:
             #    return _err(errno=40001)
 
-            log.info(f'searching for user {user_id!r}')
+            log.debug(f'searching for user {user_id!r}')
 
-            # way easier using id->raw_user instead of searching through userdb
-            raw_userdata = self.server.get_raw_user(user_id)
-            if raw_userdata is None:
+            user_to_find = self.server.get_user(user_id)
+            if user_to_find is None:
                 return _err(errno=10013)
 
-            return _json(strip_user_data(raw_userdata))
+            return _json(user_to_find.as_json)
 
     async def h_add_user(self, request):
         """`POST /users/add`.
@@ -112,23 +108,19 @@ class UsersEndpoint:
             "message": "success"
         })
 
-    async def h_patch_me(self, request):
+    @auth_route
+    async def h_patch_me(self, request, user):
         """`PATCH /users/@me`.
 
         Changes a user.
         Returns the new user object.
         """
-        _error = await self.server.check_request(request)
-        _error_json = json.loads(_error.text)
-        if _error_json['code'] == 0:
-            return _error
 
         try:
             payload = await request.json()
         except:
             return _err("error parsing")
 
-        user = self.server._user(_error_json['token'])
         new_raw_user = {}
 
         new_username = payload.get('username', user.username)
@@ -145,7 +137,8 @@ class UsersEndpoint:
 
         return _json(user.as_json)
 
-    async def h_get_me_settings(self, request):
+    @auth_route
+    async def h_get_me_settings(self, request, user):
         """`GET /users/@me/settings`.
 
         Dummy handler.
